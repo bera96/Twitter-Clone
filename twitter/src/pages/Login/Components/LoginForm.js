ce18f0297a52d469e6cf7795c62ac224be0ac144
Bera Yilmazyurt <sentinelprime33@gmail.com>
2021-10-27 23:11:04 +0300

    create login actions,types

--- twitter/src/pages/Login/Components/LoginForm.js
+++ twitter/src/pages/Login/Components/LoginForm.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Input } from '.';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { BackGroundContext } from '../../../Context/BackGroundContext';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { loginInitiate } from '../../../redux/actions/user';
 
 function LoginForm({setLogin,setLayOut}) {
     const { bird } = useContext(BackGroundContext);
     const [state,setState]=useState({
         email:"",
         name:"",
-        displayName:"",
         password:"",
-        confirmPassword:""
+      
     })
     const onClickHandler=()=>{
         setLogin(false)
         setLayOut(false)
     }
-   
-    const{email,name,password,confirmPassword,displayName}=state
+   const handleSubmit=(e)=>{
+    e.preventDefault()
+    if(!email || !password){
+        return 
+    }
+    dispatch(loginInitiate(email,password))
+    setState({email:"",password:""})
+   }
+    const{email,name,password}=state
+
+    const {currentUser} = useSelector(state=>state.user)
+    const history=useHistory()
+
+    useEffect(()=>{
+        
+        if(currentUser){
+            history.push("/")
+        }
+
+    },[currentUser,history])
+    const dispatch=useDispatch()
     return (
         <div>
-            <div className="signup-form" >
+            <div className="signup-form login-form" >
             <div className="signup-top-flex">
                 <div className="top-f1">
                     <Link to="/">
@@ -36,19 +57,12 @@ function LoginForm({setLogin,setLayOut}) {
             <div className="signup-bottom-container">
             <form >
                 <div className="signup-title-container"><span className="signup-title">Giriş Yap</span></div>
-                <div className="name-input"><Input  signUpName={name}  as="signup-input" type="text" name="İsim"  reqired/></div>
-                <div className="password-input"><Input  signUpName={name}  as="signup-input" type="password" name="Şifre" reqired/></div>
-                <div className="password-input"><Input  signUpName={name}  as="signup-input" type="password" name="Şifreyi Onayla"  reqired/></div>
+
                 <div className="email-input"><Input  signUpEmail={name}  as="signup-input" type="email" name="E-posta" reqired/></div>
-                <div className="birthday"><span className="signup-bd" >Doğum Tarihi</span></div>
-                <div><span className="signup-bd-info" >Bu, herkese açık olarak gösterilmeyecek. Bu hesap bir işletme, evcil hayvan veya başka bir şey için olsa bile kendi yaşını doğrulaman gerekir.</span></div>
-                <div className="select-box-group">
-                    
-                    <Input as="group-month" name="Ay"/>
-                    <Input as="group-day" name="Gün"/>
-                    <Input as="group-year" name="Yıl"/>
-                    
-                </div>
+                
+                <div className="password-input"><Input  signUpName={name}  as="signup-input" type="password" name="Şifre" reqired/></div>
+                
+               
                 <div className="signup-next-container" >
                     <Link to="/login">
                     <button onClick className="signup-next">Giriş Yap</button>
--- twitter/src/redux/actions/user.js
+++ twitter/src/redux/actions/user.js
@@ -12,6 +12,18 @@ const registerFail=(error)=>({
     type:types.REGISTER_FAIL,
     payload:error
 })
+
+const loginStart=()=>({
+    type:types.LOGIN_START
+})
+const loginSuccess=(user)=>({
+    type:types.LOGIN_SUCCESS,
+    payload:user
+})
+const loginFail=(error)=>({
+    type:types.LOGIN_FAIL,
+    payload:error
+})
     
 
 export const registerInitiate=(email,password,displayName,)=>{
@@ -24,5 +36,16 @@ export const registerInitiate=(email,password,displayName,)=>{
             })
             dispatch(registerSuccess(user))
         }).catch((error)=>{dispatch(registerFail(error.message))})
+    }
+}
+
+export const loginInitiate=(email,password,)=>{
+    
+    return function(dispatch){
+        dispatch(loginStart())
+        auth.signInWithEmailAndPassword(email,password).then(({user})=>{
+           
+            dispatch(loginSuccess(user))
+        }).catch((error)=>{dispatch(loginFail(error.message))})
     }
 }
